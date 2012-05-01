ca830fe17dd341f46def9241f0a4c98b69faba86
Stephen Hardisty <shardisty@etsy.com>
2012-05-01 03:00:10 +0000

    Tightened up what can be instantiated

--- MockyMockenstein/Replacement/Mock/Static.php
+++ MockyMockenstein/Replacement/Mock/Static.php
@@ -4,7 +4,7 @@ namespace MockyMockenstein;
 abstract class Replacement_Mock_Static extends Replacement_Mock {
     protected $stub_class = '\MockyMockenstein\Stub_Static';
 
-    public function willInstantiate($mock) {
+    public function willInstantiate(Replacement_Mock_Instance $mock) {
         $stub = $mock->willReceive('__construct');
         $stub->is_constructor = true;
         Router::addConstructorOverride(get_called_class(), $mock, $stub);
--- MockyMockenstein/Replacement/Spy/Static.php
+++ MockyMockenstein/Replacement/Spy/Static.php
@@ -4,7 +4,7 @@ namespace MockyMockenstein;
 class Replacement_Spy_Static extends Replacement_Spy {
     protected $stub_class = '\MockyMockenstein\Stub_Static';
 
-    public function willInstantiate($mock) {
+    public function willInstantiate(Replacement_Mock_Instance $mock) {
         $stub = $mock->willReceive('__construct');
         $stub->is_constructor = true;
         Router::addConstructorOverride($this->name, $mock, $stub);
